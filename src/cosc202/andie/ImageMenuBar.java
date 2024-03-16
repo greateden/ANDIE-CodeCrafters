ca9bd646c3cca15a0667f8b3dd23e0e28519d987
lyaan760 <89676623+Infinity585@users.noreply.github.com>
2024-03-16 14:03:05 +1300

    Working Image rotate
    Allows for user to input rotation

--- src/cosc202/andie/ImageMenuBar.java
+++ src/cosc202/andie/ImageMenuBar.java
@@ -4,6 +4,8 @@ import java.util.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+import cosc202.andie.FilterActions.GaussianFilterAction;
+
 /**
  * <p>
  * Actions provided by the Image menu.
@@ -35,6 +37,7 @@ public class ImageMenuBar {
         actions = new ArrayList<Action>();
         actions.add(new ImageMenuBarFlipHorizontal("Flip Horizontal", null, "Flips the image along the y-axis", Integer.valueOf(KeyEvent.VK_H)));
         actions.add(new ImageMenuBarFlipVertical("Flip Vertical", null, "Flips the image along the x-axis", Integer.valueOf(KeyEvent.VK_V)));
+        actions.add(new RotateImageAction("Rotate Image", null, "Apply an Image rotation at set theta", Integer.valueOf(KeyEvent.VK_M)));
     }
 
     /**
@@ -127,6 +130,62 @@ public class ImageMenuBar {
             target.repaint();
             target.getParent().revalidate();
         }
+    }
+
+    /**
+     *  Action class code layout created by Steven Mills
+     */
+    public class RotateImageAction extends ImageAction {
+
+        /**
+         * <p>
+         * Create a new Gaussian-filter action.
+         * </p>
+         * 
+         * @param name The name of the action (ignored if null).
+         * @param icon An icon to use to represent the action (ignored if null).
+         * @param desc A brief description of the action  (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         */
+        RotateImageAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+
+        /**
+         * <p>
+         * Callback for when the convert-to-grey action is triggered.
+         * </p>
+         * 
+         * <p>
+         * This method is called whenever the GaussianFilterAction is triggered.
+         * It prompts the user for a filter radius, then applys an appropriately sized {@link GaussianFilter}.
+         * </p>
+         * 
+         * @param e The event triggering this callback.
+         */
+        public void actionPerformed(ActionEvent e) {
+
+            // Determine the radius - ask the user.
+            int deg = 1;
+
+            // Pop-up dialog box to ask for the radius value.
+            SpinnerNumberModel degModel = new SpinnerNumberModel(1, 1, 360, 1);
+            JSpinner degSpinner = new JSpinner(degModel);
+            int option = JOptionPane.showOptionDialog(null, degSpinner, "Enter rotation theta", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+
+            // Check the return value from the dialog box.
+            if (option == JOptionPane.CANCEL_OPTION) {
+                return;
+            } else if (option == JOptionPane.OK_OPTION) {
+                deg = degModel.getNumber().intValue();
+            }
+
+            // Create and apply the filter
+            target.getImage().apply(new ImageRotate(deg));
+            target.repaint();
+            target.getParent().revalidate();
+        }
+
     }
 }
     
---
+++ src/cosc202/andie/ImageRotate.java
@@ -0,0 +1,69 @@
+package cosc202.andie;
+
+import java.awt.Graphics2D;
+import java.awt.image.*;
+
+/**
+ * <p>
+ * ImageOperation to invert an image's colours.
+ * </p>
+ * 
+ * <p>
+ * The images produced by this operation are still technically colour images,
+ * in that they have red, green, and blue values, but each pixel has the inverse of its value.
+ * </p>
+ * 
+ * <p>
+ * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>
+ * </p>
+ * 
+ * @author Steven Mills
+ * @version 1.0
+ */
+public class ImageRotate implements ImageOperation, java.io.Serializable {
+
+    private int deg;
+
+    ImageRotate(int deg){
+        this.deg = deg;
+    }
+    /**
+     * <p>
+     * Create a new ImageInvert operation.
+     * </p>
+     */
+    ImageRotate() {}
+
+    /**
+     * <p>
+     * Apply image invert conversion to an image.
+     * </p>
+     * 
+     * <p>
+     * 
+     * </p>
+     * 
+     * @param input The image to be inverted.
+     * @return The resulting inverted image.
+     */
+    public BufferedImage apply(BufferedImage input) {
+       
+        double rad = Math.toRadians(deg); // converts the input degress to radians 
+        int newWidth = (int) Math.floor(input.getWidth() * Math.abs(Math.cos(rad)) + input.getHeight() * Math.abs(Math.sin(rad)));
+        int newHeight = (int) Math.floor(input.getWidth() * Math.abs(Math.sin(rad)) + input.getHeight() * Math.abs(Math.cos(rad)));
+        // Image rotation Caculation from NGLN (https://math.stackexchange.com/users/17336/ngln), Rotating a rectangle, URL (version: 2017-04-13): https://math.stackexchange.com/q/71069
+
+
+        BufferedImage transformedImg = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB); // creates a new image with a new w,h to adjust for transfomration
+        Graphics2D graphics = transformedImg.createGraphics(); // creates a graphics object to redraw the image
+        graphics.translate((newWidth - input.getWidth()) /2 , (newHeight - input.getHeight()) /2); 
+        graphics.rotate(rad, input.getWidth()/2 , input.getHeight()/2);
+        graphics.drawRenderedImage(input, null); // draws the image onto the canvas
+        graphics.dispose(); // closes the graphics good practics and I belive it helps to stop memory leaks
+
+
+        
+        return transformedImg; // returns the transformed image no point returning input 
+    }
+    
+}
