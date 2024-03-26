b81d368ebc4cd1d8eb19ad785cb064987bf71836
Yuxing Zhang <zhayu940@student.oatgo.ac.nz>
2024-03-26 15:38:56 +1300

    fixing bad merge

--- src/cosc202/andie/EditActions.java
+++ src/cosc202/andie/EditActions.java
@@ -5,7 +5,6 @@ import java.awt.event.*;
 import javax.swing.*;
 
 /**
-/**
  * <p>
  * Actions provided by the Edit menu.
  * </p>
@@ -15,13 +14,8 @@ import javax.swing.*;
  * There are a lot of operations that a user might expect to see here.
  * In the sample code there are Undo and Redo actions, but more may need to be
  * added.
- * In the sample code there are Undo and Redo actions, but more may need to be
- * added.
  * </p>
  * 
- * <p>
- * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA
- * 4.0</a>
  * <p>
  * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA
  * 4.0</a>
@@ -32,9 +26,7 @@ import javax.swing.*;
  */
 public class EditActions {
 
-     ResourceBundle bundle = Andie.bundle;
-
-     /** A list of actions for the Edit menu. */
+    /** A list of actions for the Edit menu. */
     protected ArrayList<Action> actions;
 
     /**
@@ -58,7 +50,7 @@ public class EditActions {
     public JMenu createMenu() {
         JMenu editMenu = new JMenu(Andie.bundle.getString("Edit"));
 
-        for (Action action : actions) {
+
         for (Action action : actions) {
             editMenu.add(new JMenuItem(action));
         }
@@ -80,10 +72,6 @@ public class EditActions {
          * Create a new undo action.
          * </p>
          * 
-         * @param name     The name of the action (ignored if null).
-         * @param icon     An icon to use to represent the action (ignored if null).
-         * @param desc     A brief description of the action (ignored if null).
-         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
          * @param name     The name of the action (ignored if null).
          * @param icon     An icon to use to represent the action (ignored if null).
          * @param desc     A brief description of the action (ignored if null).
@@ -116,27 +104,15 @@ public class EditActions {
                             "Warning", JOptionPane.WARNING_MESSAGE);
                 }
             }
-            try {
-                target.getImage().undo();
-                target.repaint();
-                target.getParent().revalidate();
-            } catch (Exception err) {
-                if (err instanceof EmptyStackException) {
-                    JOptionPane.showMessageDialog(null, "With all due respect, you didn't open anything or this is the first step you've done.",
-                            "Warning", JOptionPane.WARNING_MESSAGE);
-                }
-            }
         }
     }
 
-    /**
     /**
      * <p>
      * Action to redo an {@link ImageOperation}.
      * </p>
      * 
      * @see EditableImage#redo()
-     */
      */
     public class RedoAction extends ImageAction {
 
@@ -149,15 +125,10 @@ public class EditActions {
          * @param icon     An icon to use to represent the action (ignored if null).
          * @param desc     A brief description of the action (ignored if null).
          * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
-         * @param name     The name of the action (ignored if null).
-         * @param icon     An icon to use to represent the action (ignored if null).
-         * @param desc     A brief description of the action (ignored if null).
-         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
          */
         RedoAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
-
 
         /**
          * <p>
@@ -172,16 +143,6 @@ public class EditActions {
          * @param e The event triggering this callback.
          */
         public void actionPerformed(ActionEvent e) {
-            try {
-                target.getImage().redo();
-                target.repaint();
-                target.getParent().revalidate();
-            } catch (Exception err) {
-                if (err instanceof EmptyStackException) {
-                    JOptionPane.showMessageDialog(null, "With all due respect, you didn't open anything or this is the last step you've done.",
-                            "Warning", JOptionPane.WARNING_MESSAGE);
-                }
-            }
             try {
                 target.getImage().redo();
                 target.repaint();
