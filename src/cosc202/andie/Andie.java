0770381309d7eb49a6a9f1e2bc2c98ecf1558d60
liyi5784 <liyi5784@student.otago.ac.nz>
2024-03-26 16:32:26 +1300

    Revert "Merge branch 'main' of https://altitude.otago.ac.nz/cosc202-c/andie"
    This reverts commit 3ce6184cb6c9cdabe52d231ca5f693cc6508820e

--- src/cosc202/andie/Andie.java
+++ src/cosc202/andie/Andie.java
@@ -176,9 +176,7 @@ public class Andie {
 
                 // Making preferences and locale
                 Preferences p = Preferences.userNodeForPackage(Andie.class);
-                //Locale.setDefault(new Locale(p.get("language", "id"), p.get("country", "ID")));
-                //System.out.println(p.get("language", "fail"));
-                Locale.setDefault(new Locale(p.get("language", "en"), p.get("country", "NZ")));
+                Locale.setDefault(new Locale(p.get("language", "id"), p.get("country", "ID")));
                 System.out.println(p.get("language", "fail"));
                 //Locale en_NZ = Locale.of("en", "NZ");
                 //Locale.setDefault(en_NZ);
